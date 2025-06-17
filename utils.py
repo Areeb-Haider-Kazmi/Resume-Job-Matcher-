e613dfd50618e2d756a1562fb1b65ff8891a44ff
Areeb-Haider-Kazmi <142672454+Areeb-Haider-Kazmi@users.noreply.github.com>
2025-06-17 18:49:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-# Download resources safely
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
